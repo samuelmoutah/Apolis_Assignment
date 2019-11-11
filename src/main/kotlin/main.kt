e8bbc57ff05fa318a422341209dc190a31bb83e1
Sungjin Mo <samuelmo78@gmail.com>
2019-11-11 01:14:54 -0600

    added matrix

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+import java.io.FileReader
 import java.util.Scanner
 
 fun main(args: Array<String>) {
@@ -49,23 +50,55 @@ fun main(args: Array<String>) {
 
 //////////////////////////Largest among 3 numbers//////////////////////////////////
 
-    println("Enter a numbers: ")
-    var input = Scanner(System.`in`)
+//    println("Enter a numbers: ")
+//    var input = Scanner(System.`in`)
+//
+//    var int1 = input.nextInt()
+//    var int2 = input.nextInt()
+//    var int3 = input.nextInt()
+//
+//    if(int1 > int2 && int1 > int3) {
+//        println("$int1 is largest")
+//    }
+//    else if(int2 > int1 && int2 > int3) {
+//        println("$int2 is largest")
+//    }
+//    else {
+//        println("$int3 is largest")
+//    }
 
-    var int1 = input.nextInt()
-    var int2 = input.nextInt()
-    var int3 = input.nextInt()
 
-    if(int1 > int2 && int1 > int3) {
-        println("$int1 is largest")
+    val row1 = 2
+    val col1 = 3
+    val row2 = 3
+    val col2 = 2
+
+    val firstMatrix = arrayOf(intArrayOf(1,2,3), intArrayOf(4,5,6))
+    val secondMatrix = arrayOf(intArrayOf(7,8), intArrayOf(9, 0), intArrayOf(0, 1))
+
+    val newMatrix = multiplyMatrices(firstMatrix, secondMatrix, row1, col1, col2)
+
+    display(newMatrix)
+}
+
+fun multiplyMatrices(firstMatrix: Array<IntArray>, secondMatrix: Array<IntArray>, row1: Int, col1: Int, col2: Int): Array<IntArray> {
+    val result = Array(row1) { IntArray(col2) }
+    for (i in 0..row1 - 1) {
+        for (j in 0..col2 - 1) {
+            for (k in 0..col1 - 1) {
+                result[i][j] += firstMatrix[i][k] * secondMatrix[k][j]
+            }
+        }
     }
-    else if(int2 > int1 && int2 > int3) {
-        println("$int2 is largest")
+    return result
+}
+
+fun display(matrix: Array<IntArray>) {
+    println("Product of two matrices is: ")
+    for (row in matrix) {
+        for (col in row) {
+            print("$col    ")
+        }
+        println()
     }
-    else {
-        println("$int3 is largest")
-    }
-
-
-
 }
